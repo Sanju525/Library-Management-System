1a4daecba9911ec4ec1c8c5d4061070a1fec68eb
Seelam Sanjay Ganesh <51526260+Sanju525@users.noreply.github.com>
2020-08-30 18:05:17 +0530

    Update views.py

--- students/views.py
+++ students/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-def login(request):
-	return render(request,'students/login.html')
+# def login(request):  # class based view can be given.
+# 	return render(request,'students/login.html')
+
+
+# @login_required(login_url='student/login/')
+# def Profile(request):
+# 	return render(request, '',{})
+
+
+# @login_required(login_url='student/login/')
+# def StudentHome(request): # contains Books added by Admin : Dict={Books}
+# 	return render(request, '', {})
