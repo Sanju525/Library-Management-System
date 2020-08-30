36dd12c871ad956f0c03072f85ca5826ae42217a
Seelam Sanjay Ganesh <51526260+Sanju525@users.noreply.github.com>
2020-08-30 18:06:15 +0530

    Update views.py

--- library_admin/views.py
+++ library_admin/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from . import forms
 # Create your views here.
 def homePage(request):
 	return render(request,'library_admin/home.html')
+
+def AdminLogin(request):
+	form = forms.AdminLoginForm()
+	if request.method == 'POST':
+		form=forms.AdminLoginForm(request.POST)
+	return render(request, 'library_admin/logs/login.html', {'form': form})
